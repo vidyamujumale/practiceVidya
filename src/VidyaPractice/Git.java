9ea67bc78c3f65505d4cd23a08ea5b076ae7ae76
vidyamujumale <vidyakhenat4@gmail.com>
2023-07-14 21:29:57 +0530

    FIle Added

---
+++ src/VidyaPractice/Git.java
@@ -0,0 +1,14 @@
+package VidyaPractice;
+
+public class Git {
+
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+}
+static public void addition(){
+	  
+	  System.out.println("Hello Everyone");
+	  }
+}
